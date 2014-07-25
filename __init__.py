9685d6bfb347fe72f6a0e2c588634d11ad23fe1f
szibis <szibis@gmail.com>
2014-07-25 21:44:33 +0200

    Initial commit - version 0.1

---
+++ __init__.py
@@ -0,0 +1,3 @@
+"Graphite-Recovery"
+
+__version__ = "0.1"
---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup, find_packages
+    extra = dict(test_suite="tests.test.suite", include_package_data=True)
+except ImportError:
+    from distutils.core import setup
+    extra = {}
+
+import recovery
+
+def readme():
+    with open("README.md") as f:
+        return f.read()
+
+setup(
+    name='graphite-recovery',
+    version=graphite-recovery.__version__,
+    description='Graphite recovery whisper files from remote machine replic',
+    long_description = readme(),
+    author='SS',
+    author_email='slawomir.skowron@gmail.com',
+    url='git@github.com:szibis/graphite-recovery.git',
+    install_requires=['argparse','Pygtail','ConfigParser','paramiko'],
+    zip_safe=False,
+    scripts=[
+        'bin/graphite-recovery'
+    ],
+    include_package_data = True,
+    packages=find_packages(),
+    license='GPL3',
+)
